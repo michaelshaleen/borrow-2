e6b7480ea39ca828c1737edd3bac928a3392d8c0
michaelshaleen <shaleenmichael@yahoo.com>
2025-09-09 16:30:11 -0500

    clean server accept changes

--- my-backend/server.js
+++ my-backend/server.js
@@ -7,10 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/api/register', (req, res) => {
-<<<<<<< HEAD
-=======
   console.log(req.body, "req.body in server.js");
->>>>>>> bbefb12a9bdcbfb078b116cd10936f5a0cfabdfc
   console.log("hit post /api/register");
   const { username, email, password } = req.body;
 
